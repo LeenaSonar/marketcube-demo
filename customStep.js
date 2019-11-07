5066270c3b6d21e7f3c7bb280391125b0a20af97
LeenaSonar <leena.sonar@successive.tech>
2019-11-07 17:51:13 +0530

    feat: save changes remove unwanted custom steps

--- customStep.js
+++ customStep.js
@@ -3,12 +3,6 @@
 
 const customActor = () => {
   const I = actor();
-  I.customClearField = async function (locator) {
-    this.click(locator);
-    this.pressKey(["control", "a"]);
-    this.pressKey("Delete")
-
-  };
   return I;
 };
 module.exports = customActor();
